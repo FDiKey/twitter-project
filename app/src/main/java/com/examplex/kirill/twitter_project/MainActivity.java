f030f83da28a84414bdbb0086050671400c54d95
Kirill Malyshev <fun.d.kira@gmail.com>
2018-07-04 18:26:56 +0300

    first commit

---
+++ app/src/main/java/com/examplex/kirill/twitter_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.examplex.kirill.twitter_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
